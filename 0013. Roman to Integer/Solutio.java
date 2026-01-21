637cd5066e349f0041e6d810f54a878b0be4f06b
Abhishek Choudhary <ab877754@gmail.com>
2026-01-21 16:07:30 +0530

    13-Another way using hashmap

--- 0013. Roman to Integer/Solutio.java
+++ 0013. Roman to Integer/Solutio.java
@@ -29,3 +29,36 @@ class Solution {
         return 0;
     }
 }
+
+//Another solution using HashMap
+
+// class Solution {
+//     public int romanToInt(String s) {
+//         HashMap<Character,Integer> map = new HashMap<>();
+
+//         map.put('I',1);
+//         map.put('V',5);
+//         map.put('X',10);
+//         map.put('L',50);
+//         map.put('C',100);
+//         map.put('D',500);
+//         map.put('M',1000);
+
+//         int res = 0;
+//         int n = s.length();
+
+//         for(int i=0;i<n;i++){
+//             int curr = map.get(s.charAt(i));
+//             int next = (i+1<n)?map.get(s.charAt(i+1)):0;
+
+//             if(curr<next){
+//                 res-=curr;
+//             }
+//             else {
+//                 res+=curr;
+//             }
+//         }
+
+//         return res;
+//     }
+// }

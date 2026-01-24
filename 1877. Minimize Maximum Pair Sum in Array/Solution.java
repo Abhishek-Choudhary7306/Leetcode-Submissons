997c49cefdf994e95a1a434a88a079c2a41efa61
Abhishek Choudhary <ab877754@gmail.com>
2026-01-24 20:57:15 +0530

    1877-daily-problem

--- 1877. Minimize Maximum Pair Sum in Array/Solution.java
+++ 1877. Minimize Maximum Pair Sum in Array/Solution.java
@@ -1,3 +1,18 @@
-public class Solution {
-    
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int i = 0;
+        int j = nums.length - 1;
+        int maxPairSum = 0;
+
+        while (i < j) {
+            int pairSum = nums[i] + nums[j];
+            maxPairSum = Math.max(maxPairSum, pairSum);
+            i++;
+            j--;
+        }
+
+        return maxPairSum;
+    }
 }

3794c983c13940684a2a11a0aaa79910372e452a
Abhishek Choudhary <ab877754@gmail.com>
2026-01-29 23:36:20 +0530

    another solution

--- 0020. Valid Parentheses/Solution.py
+++ 0020. Valid Parentheses/Solution.py
@@ -12,4 +12,25 @@ class Solution(object):
 
         return not st
 
-        
+
+#alternate solution
+
+# class Solution(object):
+#     def isValid(self, s):
+#         st = []
+
+#         for e in s:
+#             if e == '(' or e == '{' or e == '[':
+#                 st.append(e)
+#             else:
+#                 if not st:
+#                     return False
+
+#                 c = st.pop()
+
+#                 if (e == ')' and c != '(') or (e == '}' and c != '{') or (e == ']' and c != '['):
+#                     return False
+
+#         return len(st) == 0
+
+

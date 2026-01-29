a56a49ab261d581319f062c83cbe6afa9eebee25
Abhishek Choudhary <ab877754@gmail.com>
2026-01-29 23:28:41 +0530

    20

--- 0020. Valid Parentheses/Solution.java
+++ 0020. Valid Parentheses/Solution.java
@@ -1,3 +1,21 @@
-public class Solution {
-    
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st =  new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char e = s.charAt(i);
+            if(e=='('||e=='{'||e=='['){
+                st.push(e);
+            }
+
+            else{
+                if (st.isEmpty()) return false;
+
+                char c = st.pop();
+
+                if((e==')' && c!='(')||(e=='}' && c!='{')||(e==']' && c!='['))return false;
+            }
+        }
+        return st.isEmpty();
+    } 
 }

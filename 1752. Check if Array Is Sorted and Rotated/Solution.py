11f5bf3d30555f338c15400c72b0298e5f58de17
Abhishek Choudhary <ab877754@gmail.com>
2026-02-07 22:49:26 +0530

    1752 another solution

--- 1752. Check if Array Is Sorted and Rotated/Solution.py
+++ 1752. Check if Array Is Sorted and Rotated/Solution.py
@@ -14,3 +14,28 @@ class Solution(object):
                     return False
 
         return True
+    
+
+#another solution     
+# class Solution(object):
+#     def check(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         n = len(nums)
+
+#         # find index of minimum element
+#         min_index = 0
+#         for i in range(1, n):
+#             if nums[i] < nums[min_index]:
+#                 min_index = i
+
+#         # check sorted order in circular manner
+#         for i in range(n - 1):
+#             curr = nums[(min_index + i) % n]
+#             nxt = nums[(min_index + i + 1) % n]
+#             if curr > nxt:
+#                 return False
+
+#         return True

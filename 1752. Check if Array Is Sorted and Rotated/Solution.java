2f6a0445772336a78cc37c67782d879db6d30b26
Abhishek Choudhary <ab877754@gmail.com>
2026-02-07 22:48:46 +0530

    1752 another solution

--- 1752. Check if Array Is Sorted and Rotated/Solution.java
+++ 1752. Check if Array Is Sorted and Rotated/Solution.java
@@ -13,3 +13,29 @@ class Solution {
         return true;
     }
 }
+
+
+//another solution 
+// class Solution {
+//     public boolean check(int[] nums) {
+//         int n = nums.length;
+
+//         int minIndex = 0;
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] < nums[minIndex]) {
+//                 minIndex = i;
+//             }
+//         }
+
+//         // check if array is sorted from minIndex in circular way
+//         for (int i = 0; i < n - 1; i++) {
+//             int curr = nums[(minIndex + i) % n];
+//             int next = nums[(minIndex + i + 1) % n];
+//             if (curr > next) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }

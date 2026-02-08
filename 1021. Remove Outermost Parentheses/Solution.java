4c5a9d9102a19a935d589eeb28926078b47fc556
Abhishek Choudhary <ab877754@gmail.com>
2026-02-08 17:19:11 +0530

    1021

--- 1021. Remove Outermost Parentheses/Solution.java
+++ 1021. Remove Outermost Parentheses/Solution.java
@@ -1,3 +1,18 @@
-public class Solution {
-    
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int depth = 0;
+        StringBuilder res = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                if(depth>0)res.append(c);
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth>0)res.append(c);
+            }
+        }
+        return res.toString();
+    }
 }

ec6ab662d72da1845b175e0b992eb8d9c9ee5b3b
Abhishek Choudhary <ab877754@gmail.com>
2026-02-13 16:37:09 +0530

    33 better solution

--- 0033. Search in Rotated Sorted Array/Solution.py
+++ 0033. Search in Rotated Sorted Array/Solution.py
@@ -1,4 +1,18 @@
-#brute force
+# #brute force
+
+# class Solution(object):
+#     def search(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: int
+#         """
+#         for i in range(len(nums)):
+#             if target==nums[i]:
+#                 return i
+#         return -1
+
+#better solution
 
 class Solution(object):
     def search(self, nums, target):
@@ -7,7 +21,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i in range(len(nums)):
-            if target==nums[i]:
-                return i
+        n = len(nums)
+        left = 0
+        right = n-1
+
+        while left<=right:
+            mid = left + (right-left)//2
+
+            if(nums[mid]==target):
+                return mid
+            
+            if(nums[left]<=nums[mid]):
+                if(nums[left]<=target<nums[mid]):
+                    right = mid-1
+                else:
+                    left = mid+1
+
+            else:
+                if(nums[mid]<target<=nums[right]):
+                    left = mid+1
+                else:
+                    right = mid-1
         return -1
+

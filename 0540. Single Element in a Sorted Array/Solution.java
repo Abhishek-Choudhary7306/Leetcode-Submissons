046b9ba4af3e0b29bf4f04dc8fe3643a8acd9a74
Abhishek Choudhary <ab877754@gmail.com>
2026-02-14 15:52:58 +0530

    540

--- 0540. Single Element in a Sorted Array/Solution.java
+++ 0540. Single Element in a Sorted Array/Solution.java
@@ -1,3 +1,30 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        if(n==1)return nums[0];
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int l = 1;
+        int r = n-2;
+
+        while(l<=r){
+            int m = l+(r-l)/2;
+
+            if(nums[m]!=nums[m-1] && nums[m]!=nums[m+1]){
+                return nums[m];
+            }
+            //unique element at left
+            if((m%2==0 && nums[m]!=nums[m+1])||(m%2!=0 && nums[m]!=nums[m-1])){
+                r = m-1;
+            }
+            else{
+                l = m+1;
+            }
+        }
+        return -1;
+    }
+}
+
 // //brute force
 // class Solution {
 //     public int singleNonDuplicate(int[] nums) {
@@ -20,30 +47,3 @@
 //         return -1;
 //     }
 // }
-
-class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        if(n==1)return nums[0];
-        if(nums[0]!=nums[1])return nums[0];
-        if(nums[n-1]!=nums[n-2])return nums[n-1];
-        int l = 1;
-        int r = n-2;
-
-        while(l<=r){
-            int m = l+(r-l)/2;
-
-            if(nums[m]!=nums[m-1] && nums[m]!=nums[m+1]){
-                return nums[m];
-            }
-            //we have crossed the unique element
-            if((m%2==0 && nums[m]!=nums[m+1])||(m%2!=0 && nums[m]!=nums[m-1])){
-                r = m-1;
-            }
-            else{
-                l = m+1;
-            }
-        }
-        return -1;
-    }
-}

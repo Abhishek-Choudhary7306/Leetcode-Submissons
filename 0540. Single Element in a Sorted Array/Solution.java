151b0d89fc0aea08e8637aea608fbc29d49c1bbd
Abhishek Choudhary <ab877754@gmail.com>
2026-02-14 13:59:14 +0530

    540

---
+++ 0540. Single Element in a Sorted Array/Solution.java
@@ -0,0 +1,22 @@
+//brute force
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                if(nums[i]!=nums[i+1])return nums[i];
+            }
+
+            if(i==nums.length-1){
+                if(nums[i]!=nums[i-1])return nums[i];
+            }
+
+            if(i!=0 && i!=nums.length-1){
+                if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1])return nums[i];
+            }
+        }
+        return -1;
+    }
+}

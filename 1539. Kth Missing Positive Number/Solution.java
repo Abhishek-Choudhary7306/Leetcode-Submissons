dfb4c3617cd0cdcc15ebb92906fdf04b000b759f
Abhishek Choudhary <ab877754@gmail.com>
2026-02-16 23:40:34 +0530

    1539

--- 1539. Kth Missing Positive Number/Solution.java
+++ 1539. Kth Missing Positive Number/Solution.java
@@ -1,23 +1,61 @@
-//brute force
-
+//optimal solution 
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int i = 1;
-        int count = 0;
-        int j = 0;
-        
-        while(true){
-            if(j<arr.length && arr[j]==i){
-                i++;
-                j++;
+        if(arr[0]>k)return k;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left<=right) {
+            int mid = left + (right-left)/2;
+            int missing = arr[mid] - (mid + 1);
+
+            if(missing < k){
+                left = mid + 1;
             }
-            else{
-                count++;
-                if(count==k){
-                    return i;
-                }
-                i++;
+            else {
+                right = mid - 1;
             }
         }
+        return  k + (right + 1);//here right + 1 == low 
+        //so also can return low + k
     }
 }
+
+
+// //brute force
+
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int i = 1;
+//         int count = 0;
+//         int j = 0;
+        
+//         while(true){
+//             if(j<arr.length && arr[j]==i){
+//                 i++;
+//                 j++;
+//             }
+//             else{
+//                 count++;
+//                 if(count==k){
+//                     return i;
+//                 }
+//                 i++;
+//             }
+//         }
+//     }
+// }
+
+// //another solution 
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>k)return k;
+//             else{
+//                 k++;
+//             }
+//         }
+//         return k;
+//     }
+// }

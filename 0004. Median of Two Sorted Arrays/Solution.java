d2e7cae31b1845aadc9c4133d7db555e993e6bc1
Abhishek Choudhary <ab877754@gmail.com>
2026-02-20 15:12:45 +0530

    4 brute force

--- 0004. Median of Two Sorted Arrays/Solution.java
+++ 0004. Median of Two Sorted Arrays/Solution.java
@@ -36,3 +36,39 @@ class Solution {
         return 0.0; 
     }
 }
+
+//brute force solution 
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//         int n1 = nums1.length;
+//         int n2 = nums2.length;
+
+//         int[] nums3 = new int[n1+n2];
+//         int i=0,j=0,k=0;
+
+//         while(i<n1 && j<n2){
+//             if(nums1[i]<nums2[j]){
+//                 nums3[k++] = nums1[i++];
+//             }
+//             else{
+//                 nums3[k++] = nums2[j++];
+//             }
+//         }
+//         while(i<n1){
+//             nums3[k++] = nums1[i++];
+//         }
+//         while(j<n2){
+//             nums3[k++] = nums2[j++];
+//         }
+
+//         int n = nums3.length;
+//         if(n==1)return nums3[0];
+
+//         if(n%2==0){
+//             return (float)(nums3[n/2-1]+nums3[n/2])/2;
+//         }
+//         else{
+//             return nums3[n/2];
+//         }
+//     }
+// }

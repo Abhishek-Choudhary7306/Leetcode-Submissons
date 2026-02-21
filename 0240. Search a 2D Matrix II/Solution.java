c82daa35f08956b1443df7ce7e75d3f3e82c98f9
Abhishek Choudhary <ab877754@gmail.com>
2026-02-21 22:04:39 +0530

    240 better

--- 0240. Search a 2D Matrix II/Solution.java
+++ 0240. Search a 2D Matrix II/Solution.java
@@ -1,3 +1,31 @@
+//better O(m log n)
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+//         for(int i=0;i<m;i++){
+//             int left = 0;
+//             int right = n-1;
+
+//             while(left<=right){
+//                 int mid = left + (right-left)/2;
+
+//                 if(matrix[i][mid]==target){
+//                     return true;
+//                 }
+//                 else if(matrix[i][mid]<target){
+//                     left = mid + 1;
+//                 }
+//                 else{
+//                     right = mid - 1;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
 //bruteForce O(m*n) 
 // class Solution {
 //     public boolean searchMatrix(int[][] matrix, int target) {

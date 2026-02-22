bfbf9a1ed66fea7dcc462d9bac309e9d5f9db638
Abhishek Choudhary <ab877754@gmail.com>
2026-02-23 01:37:15 +0530

    240 optimal solution

--- 0240. Search a 2D Matrix II/Solution.java
+++ 0240. Search a 2D Matrix II/Solution.java
@@ -1,3 +1,29 @@
+//optimal solution O(log(m*n))
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m-1;
+
+        while(row<n && col>=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return false;
+    }
+}
+
 //better O(m log n)
 // class Solution {
 //     public boolean searchMatrix(int[][] matrix, int target) {

c8357bf3c42521ead3efb49925e4bffa022ee2a7
Abhishek Choudhary <ab877754@gmail.com>
2026-03-01 00:19:52 +0530

    205 in py another solution

--- 0205. Isomorphic Strings/Solution.py
+++ 0205. Isomorphic Strings/Solution.py
@@ -22,3 +22,25 @@ class Solution(object):
             if check2[t[i]]!=s[i]:
                 return False
         return True
+
+#another solution 
+# class Solution(object):
+#     def isIsomorphic(self, s, t):
+#         """
+#         :type s: str
+#         :type t: str
+#         :rtype: bool
+#         """
+#         if len(s)!=len(t):
+#             return False
+
+#         map1 = [0]*256
+#         map2 = [0]*256
+
+#         for i in range(len(s)):
+#             if map1[ord(s[i])]!=map2[ord(t[i])]:
+#                 return False
+#             map1[ord(s[i])] = i+1
+#             map2[ord(t[i])]= i+1
+
+#         return True

38567202872bedfe2033f42f8483a6e4346d8c11
Abhishek Choudhary <ab877754@gmail.com>
2026-03-05 19:40:29 +0530

    8

---
+++ 0008. String to Integer (atoi)/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String s) {
+        int res = 0;
+        int n = s.length();
+        int i = 0;
+        int sign = 1;
+
+        while(s.charAt(i)==' '){
+            i++;
+        }
+
+        if(i<n &&(s.charAt(i)=='+' || s.charAt(i)=='-')){
+            if(s.charAt(i)=='-'){
+                sign = -1;
+            }
+            i++;
+        }
+
+        while(i<n && Character.isDigit(s.charAt(i))){
+            int digit = s.charAt(i)-'0';
+            if(res > (Integer.MAX_VALUE - digit) / 10){
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            res = res*10 + digit;
+            i++;
+        }
+
+        return sign*res;
+    }
+}

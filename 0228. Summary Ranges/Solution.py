fab1082e7f93d0928d2a17c91043c85273773f8f
Abhishek Choudhary <ab877754@gmail.com>
2026-03-21 16:43:37 +0530

    228 in py

---
+++ 0228. Summary Ranges/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def summaryRanges(self, nums):
+        result = []
+        
+        if not nums:
+            return result
+        
+        start = nums[0]
+        
+        for i in range(1, len(nums) + 1):
+            if i == len(nums) or nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    result.append(str(start))
+                else:
+                    result.append("{}->{}".format(start, nums[i - 1]))
+                
+                if i < len(nums):
+                    start = nums[i]
+        
+        return result

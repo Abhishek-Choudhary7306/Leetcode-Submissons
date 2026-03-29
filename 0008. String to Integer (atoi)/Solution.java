89f0beb27856912f1e1b7c9fc07c4785224e04ae
Abhishek Choudhary <ab877754@gmail.com>
2026-03-29 20:11:26 +0530

    8 using recursion

--- 0008. String to Integer (atoi)/Solution.java
+++ 0008. String to Integer (atoi)/Solution.java
@@ -28,3 +28,38 @@ class Solution {
         return sign*res;
     }
 }
+
+//using recursion
+
+// class Solution {
+//     public int myAtoi(String s) {
+//         s = s.trim();
+//         if (s.isEmpty()) return 0;
+
+//         int sign = 1;
+//         int startIdx = 0;
+
+//         if (s.charAt(0) == '-') {
+//             sign = -1;
+//             startIdx = 1;
+//         } else if (s.charAt(0) == '+') {
+//             startIdx = 1;
+//         }
+
+//         return (int) recHelper(s, startIdx, 0L, sign);
+//     }
+
+//     private long recHelper(String s, int index, long currentRes, int sign) {
+//         if (index >= s.length() || !Character.isDigit(s.charAt(index))) {
+//             return currentRes * sign;
+//         }
+
+//         int digit = s.charAt(index) - '0';
+//         currentRes = currentRes * 10 + digit;
+
+//         if (sign == 1 && currentRes > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+//         if (sign == -1 && (-currentRes) < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+//         return recHelper(s, index + 1, currentRes, sign);
+//     }
+// }

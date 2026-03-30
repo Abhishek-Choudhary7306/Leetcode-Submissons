ea84030e0baa9433787db83ebbf73489ccfb45c9
Abhishek Choudhary <ab877754@gmail.com>
2026-03-30 22:06:51 +0530

    50 better solution

--- 0050. Pow(x, n)/Solution.java
+++ 0050. Pow(x, n)/Solution.java
@@ -1,3 +1,28 @@
+//better iterative solution  
+
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1;
+        long N = n;
+        if(n<0){
+            x = 1/x;
+            N = -N;
+        }
+
+        while(N>0){
+            if(N%2==1){
+                res = res*x;
+                N--;
+            }
+            else {
+                x = x*x;
+                N = N/2;
+            }
+        }
+        return res;
+    }
+}
+
 //bruteforce may cause TLE
 
 // class Solution {

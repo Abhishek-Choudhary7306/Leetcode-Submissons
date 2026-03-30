65d20af39fa66083f035829718e752e5af834263
Abhishek Choudhary <ab877754@gmail.com>
2026-03-30 20:32:55 +0530

    8 using recurion in py

--- 0008. String to Integer (atoi)/Solution.py
+++ 0008. String to Integer (atoi)/Solution.py
@@ -30,3 +30,39 @@ class Solution(object):
             i+=1
 
         return sign*res
+
+#using recursion 
+# class Solution(object):
+#     def myAtoi(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         s = s.strip()
+#         if not s:
+#             return 0
+
+#         sign = 1
+#         start_idx = 0
+
+#         if s[0] == '-':
+#             sign = -1
+#             start_idx = 1
+#         elif s[0] == '+':
+#             start_idx = 1
+
+#         return int(self.rec_helper(s, start_idx, 0, sign))
+
+#     def rec_helper(self, s, index, current_res, sign):
+#         if index >= len(s) or not s[index].isdigit():
+#             return current_res * sign
+
+#         digit = ord(s[index]) - ord('0')
+#         current_res = current_res * 10 + digit
+
+#         if sign == 1 and current_res > 2**31 - 1:
+#             return 2**31 - 1
+#         if sign == -1 and -current_res < -2**31:
+#             return -2**31
+
+#         return self.rec_helper(s, index + 1, current_res, sign)

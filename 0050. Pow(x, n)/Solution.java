a814934bf2df7c9c566412b975856a102885b2b9
Abhishek Choudhary <ab877754@gmail.com>
2026-03-30 21:02:28 +0530

    50 bruteforce

---
+++ 0050. Pow(x, n)/Solution.java
@@ -0,0 +1,17 @@
+//bruteforce may cause TLE
+// class Solution {
+//     public double myPow(double x, int n) {
+//         int sign = 1;
+//         if(n<0)sign = -1;
+
+//         long n1 = Math.abs(n);
+//         int i = 0;
+//         double ans = 1;
+//         while(i<n1){
+//             ans*=x;
+//             i++;
+//         }
+
+//         return (sign==-1)?(1/ans):ans;
+//     }
+// }

cb9df4fa3816bf5b414033afe4c506817df8a702
Abhishek Choudhary <ab877754@gmail.com>
2026-03-31 13:18:00 +0530

    50 using recursion in py

--- 0050. Pow(x, n)/Solution.py
+++ 0050. Pow(x, n)/Solution.py
@@ -25,6 +25,40 @@ class Solution(object):
                 N=N/2
         
         return res
+        
+#using recursion
+
+# class Solution(object):
+#     def myPow(self, x, n):
+#         """
+#         :type x: float
+#         :type n: int
+#         :rtype: float
+#         """
+
+#         N = n 
+#         if N<0:
+#             N = -N
+#             x = 1/x
+
+#         return self.power(x,N)
+
+
+
+#     def power(self,x,n):
+#         if n==0:
+#             return 1
+
+#         half = self.power(x,n//2)
+
+#         if n%2:
+#             return x * half * half
+#         else:
+#             return half*half
+
+        
+
+        
 
         
 

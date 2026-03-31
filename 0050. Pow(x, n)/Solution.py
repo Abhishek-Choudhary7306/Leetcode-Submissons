c344985b6885a64f792c3d97207ec5bf9d1cb267
Abhishek Choudhary <ab877754@gmail.com>
2026-03-31 13:03:05 +0530

    50 better solution in py

--- 0050. Pow(x, n)/Solution.py
+++ 0050. Pow(x, n)/Solution.py
@@ -1,3 +1,33 @@
+#better iterative solution  
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        N = n
+        res = 1
+
+        if(N<0):
+            N = -N
+            x = 1/x
+
+        while N>0:
+            if(N%2==1):
+                res = res*x
+                N-=1
+
+            else:
+                x = x*x
+                N=N/2
+        
+        return res
+
+        
+
 # bruteforce may cause TLE
 
 # class Solution(object):

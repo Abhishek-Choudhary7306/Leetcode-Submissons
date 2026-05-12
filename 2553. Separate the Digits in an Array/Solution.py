7eb8d7d9462a005396a4880e65a8163633ebf82e
Abhishek Choudhary <ab877754@gmail.com>
2026-05-12 20:36:31 +0530

    2553 in py

--- 2553. Separate the Digits in an Array/Solution.py
+++ 2553. Separate the Digits in an Array/Solution.py
@@ -1,3 +1,4 @@
+#another approach
 class Solution(object):
     def separateDigits(self, nums):
         """
@@ -5,26 +6,44 @@ class Solution(object):
         :rtype: List[int]
         """
         res = []
-
-        for i in range(len(nums)):
-            curr = self.divide(nums[i])
-
-            for j in range(len(curr)):
-                res.append(curr[j])
-
-
+        for i in nums:
+            for d in str(i):
+                res.append(int(d))
+     
         return res
+        
 
-    def divide(self,num):
-        if num==0:
-            return [0]
 
-        res = []
-        while(num):
-            d = num%10
-            num = num//10
-            res.append(d)
 
-        res.reverse()
-        return res
+#first approach
+
+# class Solution(object):
+#     def separateDigits(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         res = []
+
+#         for i in range(len(nums)):
+#             curr = self.divide(nums[i])
+
+#             for j in range(len(curr)):
+#                 res.append(curr[j])
+
+
+#         return res
+
+#     def divide(self,num):
+#         if num==0:
+#             return [0]
+
+#         res = []
+#         while(num):
+#             d = num%10
+#             num = num//10
+#             res.append(d)
+
+#         res.reverse()
+#         return res
 

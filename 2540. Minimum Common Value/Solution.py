6ca056575ae2197d4cf47c8732f3635e11880f76
Abhishek Choudhary <ab877754@gmail.com>
2026-05-19 21:37:56 +0530

    2540 in py

--- 2540. Minimum Common Value/Solution.py
+++ 2540. Minimum Common Value/Solution.py
@@ -1,5 +1,4 @@
-
-#BruteForce Solution
+#Optimal Solution 
 class Solution(object):
     def getCommon(self, nums1, nums2):
         """
@@ -7,27 +6,50 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        for num in nums1:
-            if(self.bsearch(nums2,num)):
-                return num
+        i = 0
+        j = 0
 
-        return -1
+        while i<len(nums1) and j<len(nums2):
+            if(nums1[i]==nums2[j]):
+                return nums1[i]
 
-    def bsearch(self,nums,tar):
-        left = 0
-        right = len(nums) - 1
-
-        while(left<=right):
-            mid = (left+(right-left)//2)
-
-            if(nums[mid]==tar):
-                return True
-
-            elif(nums[mid]<tar):
-                left = mid+1
-
+            elif(nums1[i]>nums2[j]):
+                j+=1
             else:
-                right = mid-1
+                i+=1
+        return -1
+        
 
-        return False
+
+# #BruteForce Solution
+# class Solution(object):
+#     def getCommon(self, nums1, nums2):
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: int
+#         """
+#         for num in nums1:
+#             if(self.bsearch(nums2,num)):
+#                 return num
+
+#         return -1
+
+#     def bsearch(self,nums,tar):
+#         left = 0
+#         right = len(nums) - 1
+
+#         while(left<=right):
+#             mid = (left+(right-left)//2)
+
+#             if(nums[mid]==tar):
+#                 return True
+
+#             elif(nums[mid]<tar):
+#                 left = mid+1
+
+#             else:
+#                 right = mid-1
+
+#         return False
         

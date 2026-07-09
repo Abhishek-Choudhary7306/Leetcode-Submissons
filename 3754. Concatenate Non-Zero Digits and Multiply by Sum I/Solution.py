709be422cd876890cb3b49b41c8bb01f520db7a5
Abhishek Choudhary <ab877754@gmail.com>
2026-07-10 00:14:33 +0530

    3754 in py better

--- 3754. Concatenate Non-Zero Digits and Multiply by Sum I/Solution.py
+++ 3754. Concatenate Non-Zero Digits and Multiply by Sum I/Solution.py
@@ -1,21 +1,47 @@
+#better Solution
 class Solution(object):
     def sumAndMultiply(self, n):
         """
         :type n: int
         :rtype: int
         """
-        num = str(n)
-        res = ""
-        digitsum = 0
-
-        for i in num:
-            digit = int(i)
-            digitsum += digit
-
-            if digit!=0:
-                res+=i  
-
-        if res=="":
+        if n == 0:
             return 0
 
-        return int(res)*digitsum
+        digit_sum = 0
+        concat = 0
+        place = 1
+
+        while n > 0:
+            digit = n % 10
+            digit_sum += digit
+
+            if digit != 0:
+                concat = digit * place + concat
+                place *= 10
+
+            n //= 10
+
+        return concat * digit_sum
+
+# class Solution(object):
+#     def sumAndMultiply(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+#         num = str(n)
+#         res = ""
+#         digitsum = 0
+
+#         for i in num:
+#             digit = int(i)
+#             digitsum += digit
+
+#             if digit!=0:
+#                 res+=i  
+
+#         if res=="":
+#             return 0
+
+#         return int(res)*digitsum

83d534e406ea148acae82e81b974569f28955d17
Abhishek Choudhary <ab877754@gmail.com>
2026-07-25 19:44:15 +0530

    3536 in py

--- 3536. Maximum Product of Two Digits/Solution.py
+++ 3536. Maximum Product of Two Digits/Solution.py
@@ -1,2 +1,32 @@
 #better Solution
 
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        num = n
+        max1 = float('-inf')
+        max2 = float('-inf')
+
+        while(num>0):
+            d = num%10
+            if(d>max1):
+                max2 = max1
+                max1 = d
+
+            elif(d==max1):
+                max2 = max1
+            
+            elif(d>max2):
+                max2 = d
+            
+            num = num/10
+
+        return max1*max2
+
+
+
+        

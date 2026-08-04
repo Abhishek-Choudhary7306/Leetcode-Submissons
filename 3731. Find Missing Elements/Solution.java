c4ce49f0a2dbd551ded3035770643682a3635a64
Abhishek Choudhary <ab877754@gmail.com>
2026-08-04 21:32:54 +0530

    3731

--- 3731. Find Missing Elements/Solution.java
+++ 3731. Find Missing Elements/Solution.java
@@ -1,1 +1,31 @@
-//one solution
+//one solutionclass Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min){
+                min = nums[i];
+            }
+            if(nums[i]>max){
+                max = nums[i];
+            }
+        }
+
+        for(int i=min;i<=max;i++){
+            if(notIn(i,nums)){
+                res.add(i);
+            }
+        }
+
+        return res;
+    }
+    private boolean notIn(int n,int[] nums){
+        for(int i=0;i<nums.length;i++){
+            if(n==nums[i])return false;
+        }
+        return true;
+    }
+}

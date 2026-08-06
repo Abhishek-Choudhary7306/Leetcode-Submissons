1830341cfe4a27f6ccccd4a87e096aecf73c82cc
Abhishek Choudhary <ab877754@gmail.com>
2026-08-06 13:30:33 +0530

    3731 another solution in py

--- 3731. Find Missing Elements/Solution.py
+++ 3731. Find Missing Elements/Solution.py
@@ -1,3 +1,23 @@
+#solution using Enumeration
+
+class Solution(object):
+    def findMissingElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = sorted(nums)
+
+        res = []
+
+        for i in range(0,len(nums)-1):
+            for j in range(nums[i]+1,nums[i+1]):
+                res.append(j)
+
+        return res
+
+
+
 #one solution 
 
 # class Solution(object):

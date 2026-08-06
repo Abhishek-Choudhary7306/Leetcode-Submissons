a3481aef31b3d9ddfd6c65e79b290c8d67aac302
Abhishek Choudhary <ab877754@gmail.com>
2026-08-06 13:04:52 +0530

    minor adjustments

--- 3731. Find Missing Elements/Solution.py
+++ 3731. Find Missing Elements/Solution.py
@@ -1,16 +1,18 @@
-class Solution(object):
-    def findMissingElements(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        minE = min(nums)
-        maxE = max(nums)
+#one solution 
 
-        res = []
+# class Solution(object):
+#     def findMissingElements(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         minE = min(nums)
+#         maxE = max(nums)
 
-        for i in range(minE,maxE+1):
-            if i not in nums:
-                res.append(i)
+#         res = []
 
-        return res
+#         for i in range(minE,maxE+1):
+#             if i not in nums:
+#                 res.append(i)
+
+#         return res

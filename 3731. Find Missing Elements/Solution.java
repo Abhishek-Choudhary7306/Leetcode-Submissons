8562f0fa16e8d9c029fd71ba26e90573dfc81dc0
Abhishek Choudhary <ab877754@gmail.com>
2026-08-06 13:03:33 +0530

    3731 another solution

--- 3731. Find Missing Elements/Solution.java
+++ 3731. Find Missing Elements/Solution.java
@@ -1,32 +1,49 @@
-//one solution
+//soultion using Enumeration
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+
         List<Integer> res = new ArrayList<>();
 
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<min){
-                min = nums[i];
-            }
-            if(nums[i]>max){
-                max = nums[i];
-            }
-        }
-
-        for(int i=min;i<=max;i++){
-            if(notIn(i,nums)){
-                res.add(i);
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=nums[i]+1;j<nums[i+1];j++){
+                res.add(j);
             }
         }
 
         return res;
     }
-    private boolean notIn(int n,int[] nums){
-        for(int i=0;i<nums.length;i++){
-            if(n==nums[i])return false;
-        }
-        return true;
-    }
 }
+
+//one solution
+// class Solution {
+//     public List<Integer> findMissingElements(int[] nums) {
+//         List<Integer> res = new ArrayList<>();
+
+//         int max = Integer.MIN_VALUE;
+//         int min = Integer.MAX_VALUE;
+
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]<min){
+//                 min = nums[i];
+//             }
+//             if(nums[i]>max){
+//                 max = nums[i];
+//             }
+//         }
+
+//         for(int i=min;i<=max;i++){
+//             if(notIn(i,nums)){
+//                 res.add(i);
+//             }
+//         }
+
+//         return res;
+//     }
+//     private boolean notIn(int n,int[] nums){
+//         for(int i=0;i<nums.length;i++){
+//             if(n==nums[i])return false;
+//         }
+//         return true;
+//     }
+// }

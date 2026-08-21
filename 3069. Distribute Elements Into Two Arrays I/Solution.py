83af7665314a736b6bf7fb77cec2b053f7979b5c
Abhishek Choudhary <ab877754@gmail.com>
2026-08-21 19:50:14 +0530

    3069 in py

---
+++ 3069. Distribute Elements Into Two Arrays I/Solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def resultArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        l1 = [nums[0]]
+        l2 = [nums[1]]
+
+        for i in range(2,len(nums)):
+            if(l1[len(l1)-1]>l2[len(l2)-1]):
+                l1.append(nums[i])
+            else:
+                l2.append(nums[i])
+
+        return l1+l2
+
+
+
+

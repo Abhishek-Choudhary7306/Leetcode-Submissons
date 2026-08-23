4133dc3a78cf8f776cb9fb5de835abff62a7a271
Abhishek Choudhary <ab877754@gmail.com>
2026-08-23 18:50:55 +0530

    217 in py

---
+++ 0217. Contains Duplicate/Solution.py
@@ -0,0 +1,16 @@
+#using set
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set()
+
+        for num in nums:
+            if num in s :
+                return True
+            s.add(num)
+
+        return False

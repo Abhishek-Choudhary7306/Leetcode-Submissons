4dc8462434858d3a49b1494712c122efbd67d153
Abhishek Choudhary <ab877754@gmail.com>
2026-08-26 00:46:29 +0530

    152 optimal Solution in Java

--- 0152. Maximum Product Subarray/Solution.java
+++ 0152. Maximum Product Subarray/Solution.java
@@ -1,3 +1,28 @@
+//Optimal Soluiton 
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int res = Integer.MIN_VALUE;
+        int n = nums.length;
+
+        for(int i=0;i<n;i++){
+            if(prefix == 0)prefix = 1;
+            if(suffix == 0)suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            res = Math.max(res,Math.max(prefix,suffix));
+        }
+
+        return res;
+
+    }
+}
+
+
 // brute force Solution 
 
 // class Solution {

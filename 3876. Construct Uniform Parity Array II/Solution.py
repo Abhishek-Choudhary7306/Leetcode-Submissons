f820ce208f64b8212242f3f3abf0ec27ec527dae
Abhishek Choudhary <ab877754@gmail.com>
2026-09-03 12:50:25 +0530

    3876 in py better Soln

--- 3876. Construct Uniform Parity Array II/Solution.py
+++ 3876. Construct Uniform Parity Array II/Solution.py
@@ -1,30 +1,54 @@
+#Better Solution
+
 class Solution(object):
     def uniformArray(self, nums1):
         """
         :type nums1: List[int]
         :rtype: bool
         """
-
+        
+        minEven = float('inf')
         minOdd = float('inf')
-        nums2 = [0]*len(nums1)
 
-        for i in range(len(nums1)):
-            if(nums1[i]%2 != 0):
-                minOdd = min(nums1[i],minOdd)
+        for num in nums1:
+            if num%2==0:
+                minEven = min(minEven,num)
+            else:
+                minOdd = min(minOdd,num)
+            
+
+        return minOdd == float('inf') or minEven>minOdd
+
+
+#One Solution 
+
+# class Solution(object):
+#     def uniformArray(self, nums1):
+#         """
+#         :type nums1: List[int]
+#         :rtype: bool
+#         """
+
+#         minOdd = float('inf')
+#         nums2 = [0]*len(nums1)
+
+#         for i in range(len(nums1)):
+#             if(nums1[i]%2 != 0):
+#                 minOdd = min(nums1[i],minOdd)
 
         
-        if(minOdd == float('inf')):
-            return True
+#         if(minOdd == float('inf')):
+#             return True
 
-        for i in range(len(nums1)):
-            if(nums1[i]%2==0):
-                nums2[i] = nums1[i] - minOdd
+#         for i in range(len(nums1)):
+#             if(nums1[i]%2==0):
+#                 nums2[i] = nums1[i] - minOdd
 
-        for num in nums2:
-            if(num<0):
-                return False
+#         for num in nums2:
+#             if(num<0):
+#                 return False
 
-        return True
+#         return True
 
 
         

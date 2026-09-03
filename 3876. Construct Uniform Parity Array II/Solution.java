f249d64c2fde9178995ea080d586e5b07495d14a
Abhishek Choudhary <ab877754@gmail.com>
2026-09-03 13:18:35 +0530

    3876 in java best Soln

--- 3876. Construct Uniform Parity Array II/Solution.java
+++ 3876. Construct Uniform Parity Array II/Solution.java
@@ -1,3 +1,27 @@
-public class Solution {
-    
+//best Solution
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+        //find min Value 
+        int min = Integer.MAX_VALUE;
+        for(int num:nums1){
+            min = Math.min(min,num);
+        }
+
+        //if min value is odd the ans is always True
+        if (min%2!=0){
+            return true;
+        }
+
+
+        //if min value is even we need to check all the elements should be even 
+        for(int num : nums1){
+            if(num%2!=0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }

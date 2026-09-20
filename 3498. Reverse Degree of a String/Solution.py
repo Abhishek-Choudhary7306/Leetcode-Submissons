776e6379dfe8473d3743c5b20202e39578718e22
Abhishek Choudhary <ab877754@gmail.com>
2026-09-20 12:55:10 +0530

    3498 in py better

--- 3498. Reverse Degree of a String/Solution.py
+++ 3498. Reverse Degree of a String/Solution.py
@@ -1,14 +1,32 @@
+#better solution without using extra space
+
 class Solution(object):
     def reverseDegree(self, s):
         """
         :type s: str
         :rtype: int
         """
-        values = {'a':26, 'b':25, 'c':24, 'd':23, 'e':22, 'f':21, 'g':20, 'h':19, 'i':18, 'j':17, 'k':16, 'l':15, 'm':14, 'n':13, 'o':12, 'p':11, 'q':10, 'r':9, 's':8, 't':7, 'u':6, 'v':5, 'w':4, 'x':3, 'y':2, 'z':1}
-
-
+       
         ans = 0
+
         for i in range(len(s)):
-            ans += values[s[i]]*(i+1)
+            ans += (123-ord(s[i]))*(i+1)
 
         return ans
+
+#Solution using Map
+
+# class Solution(object):
+#     def reverseDegree(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         values = {'a':26, 'b':25, 'c':24, 'd':23, 'e':22, 'f':21, 'g':20, 'h':19, 'i':18, 'j':17, 'k':16, 'l':15, 'm':14, 'n':13, 'o':12, 'p':11, 'q':10, 'r':9, 's':8, 't':7, 'u':6, 'v':5, 'w':4, 'x':3, 'y':2, 'z':1}
+
+
+#         ans = 0
+#         for i in range(len(s)):
+#             ans += values[s[i]]*(i+1)
+
+#         return ans
